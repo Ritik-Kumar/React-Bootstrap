b8c56f9b4bd0529e47ce1d484af33ba123e23dfc
Ritik <ritikkumarrcf@gmail.com>
2020-05-12 11:53:20 +0530

    Controlled Form

---
+++ src/components/Dishdetail.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom'
+
+const RenderDish = ({ dish }) => {
+  return (
+    <div className="col-12 col-md-5 m-1">
+      <Card>
+        <CardImg top src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  )
+};
+
+
+const RenderComments = ({ comments }) => {
+  const comment = comments.map((comment) => {
+    return (
+      <li key={comment.id}>
+        <p>{comment.comment}</p>
+        <p> --{comment.author}, {new Intl.DateTimeFormat('en-us', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
+      </li>
+    )
+  }
+  );
+
+  if (comments.length > 0) {
+    return (
+      <div className="col-12 col-md-5 m-1">
+        <h4>Comments</h4>
+        <ul className="list-unstyled">
+          {comment}
+        </ul>
+      </div>
+    )
+  }
+};
+
+const Dishdetail = ({ dish, comments }) => {
+  return (
+
+    <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+          <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12"><h3>{dish.name}</h3></div>
+      </div>
+      <div className="row">
+        <RenderDish dish={dish} />
+        <RenderComments comments={comments} />
+      </div>
+    </div>
+  );
+}
+
+export default Dishdetail;
